7656f86d5088039f29faa813e965d1851b88c71b
eamangino <eam215@miami.edu>
2015-11-28 20:20:00 -0500

    Update Table 2-9 Undergrad and Fresh Eng Enroll.R

--- Table 2-9 Undergrad and Fresh Eng Enroll.R
+++ Table 2-9 Undergrad and Fresh Eng Enroll.R
@@ -13,3 +13,23 @@ colnames(FreshEngEnroll)<-namedata
 #Table 2-9 undergrad engineering total enrollment and freshmen enrollment
 UndergradEngEnroll
 FreshEngEnroll
+
+#this is what i've come up with for the charts
+
+library(reshape2)
+ugee_long <- melt(UndergradEngEnroll,
+                 id.vars = "Year and enrollment status",
+                 measure.vars = c("Female","Male"),
+                 variable.name = "gender")
+ugee_long$`Year and enrollment status`<-as.factor(ugee_long$`Year and enrollment status`)
+
+ggplot(ugee_long, aes(x=gender, y=value, colour=`Year and enrollment status`, group=`Year and enrollment status`)) +
+  geom_bar(position=position_dodge(), stat="identity") 
+
+fee_long<-melt(FreshEngEnroll,
+               id.vars = "Year and enrollment status",
+               measure.vars = c("Female","Male"),
+               variable.name = "gender")
+fee_long$`Year and enrollment status`<-as.factor(fee_long$`Year and enrollment status`)
+ggplot(fee_long, aes(x=gender, y=value, colour=`Year and enrollment status`, group=`Year and enrollment status`)) +
+  geom_bar(position=position_dodge(), stat="identity") 
